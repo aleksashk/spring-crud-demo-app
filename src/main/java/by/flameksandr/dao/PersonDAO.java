8a0c86b5171700f18c32102c568f1099f51f33f0
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-09-28 22:02:18 +0300

    refactoring CRUD app

--- src/main/java/by/flameksandr/dao/PersonDAO.java
+++ src/main/java/by/flameksandr/dao/PersonDAO.java
@@ -31,4 +31,19 @@ public class PersonDAO {
                 .findAny()
                 .orElse(null);
     }
+
+    public void save(Person person) {
+        person.setId(++PEOPLE_COUNT);
+        people.add(person);
+    }
+
+    public void update(int id, Person updatedPerson) {
+        Person personToBeUpdated = show(id);
+
+        personToBeUpdated.setName(updatedPerson.getName());
+    }
+
+    public void delete(int id) {
+        people.removeIf(p -> p.getId() == id);
+    }
 }
