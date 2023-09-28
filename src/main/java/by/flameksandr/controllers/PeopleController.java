43700710862e074d85783d56821584619ec7171c
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-09-28 21:26:53 +0300

    add methods edit, update, delete and edit.html

--- src/main/java/by/flameksandr/controllers/PeopleController.java
+++ src/main/java/by/flameksandr/controllers/PeopleController.java
@@ -1,11 +1,10 @@
 package by.flameksandr.controllers;
 
 import by.flameksandr.dao.PersonDAO;
+import by.flameksandr.models.Person;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/people")
@@ -28,5 +27,34 @@ public class PeopleController {
         model.addAttribute("person", personDAO.show(id));
 
         return "people/show";
+    }
+
+    @GetMapping("/new")
+    public String newPerson(@ModelAttribute("person") Person person) {
+        return "people/new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("person") Person person) {
+        personDAO.save(person);
+        return "redirect:/people";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String edit(Model model, @PathVariable("id") int id) {
+        model.addAttribute("person", personDAO.show(id));
+        return "people/edit";
+    }
+
+    @PatchMapping("/{id}")
+    public String update(@ModelAttribute("person") Person person, @PathVariable("id") int id) {
+        personDAO.update(id, person);
+        return "redirect:/people";
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") int id) {
+        personDAO.delete(id);
+        return "redirect:/people";
     }
 }
